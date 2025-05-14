6e00c546be6a42c0d6f8f129a348457961821c11
dynngz <dayanagzrd@gmail.com>
2025-05-14 15:42:06 -0500

    Category mapeo

---
+++ src/main/java/com/example/model/Category.java
@@ -0,0 +1,21 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="categories")
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name="name", nullable=false, length=100)
+    private String name;
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+}
