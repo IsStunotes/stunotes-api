7ace9fe52399fc70087c02f30e17056551530841
dynngz <dayanagzrd@gmail.com>
2025-07-01 17:35:27 -0500

    feat: add activity implementation with userId filter

--- src/main/java/com/example/repository/ActivityRepository.java
+++ src/main/java/com/example/repository/ActivityRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ActivityRepository extends JpaRepository<Activity, Integer> {
     Page<Activity> findByCategory_Name(String categoryName, Pageable pageable);
     Page<Activity> findAllByOrderByPriorityAsc(Pageable pageable);
-
+    Page<Activity> findByUserId(Integer userId, Pageable pageable);
+    Page<Activity> findByUserIdAndCategory_Name(Integer userId, String categoryName, Pageable pageable);
+    Page<Activity> findByUserIdOrderByPriority(Integer userId, Pageable pageable);
 }
 
